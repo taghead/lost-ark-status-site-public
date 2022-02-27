3f6b24df1e738728a16befc0641a5e06525258fe
Andrew Chander <13403032+taghead@users.noreply.github.com>
2022-02-28 00:12:31 +1100

    Added new config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NEXT_PUBLIC_VERCEL_URL: process.env.NEXT_PUBLIC_VERCEL_URL,
+  },
   images: {
     domains: ["storage.ko-fi.com", "cdn.buymeacoffee.com"],
   },
