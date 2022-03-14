8aceb3494fe4bd1b940c92efc0c13593ed96c042
Andrew Chander <13403032+taghead@users.noreply.github.com>
2022-03-14 18:05:16 +1100

    Added lang to html

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,16 @@ import { NavBar } from "../components/NavBar";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen max-h-screen min-w-screen max-w-screen flex flex-col">
-      <NavBar />
-      <div className="relative flex flex-grow h-96">
-        <main className="bg-gray-100 flex-1 p-6">
-          <Component {...pageProps} />
-        </main>
+    <html lang="en">
+      <div className="min-h-screen max-h-screen min-w-screen max-w-screen flex flex-col">
+        <NavBar />
+        <div className="relative flex flex-grow h-96">
+          <main className="bg-gray-100 flex-1 p-6">
+            <Component {...pageProps} />
+          </main>
+        </div>
       </div>
-    </div>
+    </html>
   );
 }
 
